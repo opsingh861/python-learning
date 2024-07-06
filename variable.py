413d18a26bc135d10d5544656d2bf0c2df9fcee0
Aditya Dhanraj <opsingh861@gmail.com>
2024-07-06 22:56:43 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,17 @@
+a = 2
+b = 3
+# here a and b are variables
+print(a + b)
+
+# invalid variable names
+# 2myvar = 2
+# my-var = 2
+# my var = 2
+
+# valid variable names
+my_var = 2
+myVar = 2
+_my_var = 2
+myVar2 = 2
+MYVAR = 2
+

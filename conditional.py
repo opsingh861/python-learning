98d59364949bc74be386b190fcd3ab266d271a58
Aditya Dhanraj <opsingh861@gmail.com>
2024-07-07 00:49:59 +0530

    conditionals

---
+++ conditional.py
@@ -0,0 +1,26 @@
+# conditionals ladder
+
+a = 10
+
+if a > 10:
+    print("a is greater than 10")
+elif a < 10:
+    print("a is less than 10")
+else:
+    print("a is equal to 10")
+
+
+a = "Hello world"
+
+if "world" in a:
+    print("world is present in a")
+else:
+    print("world is not present in a")
+
+
+# we can also write
+
+if ("world" in a):
+    print("world is present in a")
+else:
+    print("world is not present in a")

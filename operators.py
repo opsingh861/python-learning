745c047f8ffc9e4575c97bb9ecad065f13fa0138
Aditya Dhanraj <opsingh861@gmail.com>
2024-07-06 22:57:26 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,104 @@
+# arithmetic operators
+# +, -, *, /, %, **, // 
+
+# + operator
+a = 10
+b = 20
+c = a + b
+print(c)
+
+# - operator
+a = 10
+b = 20
+c = a - b
+print(c)
+
+# * operator
+a = 10
+b = 20
+c = a * b
+print(c)
+
+# / operator
+a = 10
+b = 20
+c = a / b
+print(c)
+
+# % operator
+a = 10
+b = 20
+c = a % b
+print(c)
+
+# ** operator (exponentiation)
+a = 10
+b = 2
+c = a ** b
+print(c)
+
+# // operator (floor division, returns the integer part of the division)
+a = 13
+b = 2
+c = a // b
+print(c)
+
+# comparison operators
+# ==, !=, >, <, >=, <=
+
+# == operator
+a = 10
+b = 20
+c = a == b
+print(c)
+
+# != operator
+a = 10
+b = 20
+c = a != b
+print(c)
+
+# > operator
+a = 10
+b = 20
+c = a > b
+print(c)
+
+# < operator
+a = 10
+b = 20
+c = a < b
+print(c)
+
+# >= operator
+a = 10
+b = 20
+c = a >= b
+print(c)
+
+# <= operator
+a = 10
+b = 20
+c = a <= b
+print(c)
+
+# logical operators
+# and, or, not
+
+# and operator
+a = 10
+b = 20
+c = 30
+d = a < b and b < c
+print(d)
+
+# or operator
+a = True
+b = False
+c = a or b
+print(c)
+
+# not operator
+a = True
+b = not a
+print(b)

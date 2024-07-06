3ca539cb83bfe6df44369f45efb6de2dc310096f
Aditya Dhanraj <opsingh861@gmail.com>
2024-07-06 22:57:40 +0530

    input

---
+++ input.py
@@ -0,0 +1,9 @@
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+c = a + b
+print(c) # this wil give us a concatenated string, because input() takes string as input
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = a + b
+print(c) # this will give us the sum of the two numbers, because we have converted the input to integer

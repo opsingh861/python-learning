cf9f7979d34fbb07f99d253f7307a856117416e2
Aditya Dhanraj <opsingh861@gmail.com>
2024-07-06 22:58:06 +0530

    os module

---
+++ os_module.py
@@ -0,0 +1,9 @@
+import os
+
+# Get the current working directory
+print(os.getcwd())
+
+cwd = os.getcwd()
+
+# print the files and directories in the current working directory
+print(os.listdir(cwd))
